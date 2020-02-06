de1a7d03893d1a9f9ca19135364dab86b2649580
aminb <amin.burhan@gmail.com>
2020-02-06 18:31:15 +0100

    Updates

---
+++ precache-manifest.6f90b21b84f97cae93eeee8baabfd53a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4bb18ce6f4f016ba8f3b193070cdb4",
+    "url": "/react-assignment-taniarascia/index.html"
+  },
+  {
+    "revision": "42ab69fac2c9b1a88310",
+    "url": "/react-assignment-taniarascia/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "941c190e4b52d286ad07",
+    "url": "/react-assignment-taniarascia/static/js/2.eb0022db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-assignment-taniarascia/static/js/2.eb0022db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ab69fac2c9b1a88310",
+    "url": "/react-assignment-taniarascia/static/js/main.356debcd.chunk.js"
+  },
+  {
+    "revision": "867a39a9a2633484e0d2",
+    "url": "/react-assignment-taniarascia/static/js/runtime-main.60fc6dc3.js"
+  }
+]);
